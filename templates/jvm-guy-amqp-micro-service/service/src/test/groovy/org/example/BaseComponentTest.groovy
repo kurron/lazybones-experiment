4de4142eed30ae9d9d16d04d9205ecdd043300fe
Ron Kurr <rkurr@transparent.com>
2014-08-16 01:02:28 -0400

    Added JDepend report.

---
+++ templates/jvm-guy-amqp-micro-service/service/src/test/groovy/org/example/BaseComponentTest.groovy
@@ -0,0 +1,10 @@
+package org.example
+
+/**
+ * Common base class for all component-level tests. Sometimes knows as integration tests,
+ * these tests access the file system, network and other external resources but don't test
+ * the entire system end-to-end from a user's perspective like a functional acceptance test
+ * does.
+ */
+class BaseComponentTest extends BaseSpecification {
+}

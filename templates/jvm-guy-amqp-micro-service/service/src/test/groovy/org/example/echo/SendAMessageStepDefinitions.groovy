866b3920ae7315b089ed9843bf30df4efe0e988d
Ron Kurr <rkurr@transparent.com>
2014-08-16 22:44:19 -0400

    Cucumber seems to be working.

--- templates/jvm-guy-amqp-micro-service/service/src/test/groovy/org/example/echo/SendAMessageStepDefinitions.groovy
+++ templates/jvm-guy-amqp-micro-service/service/src/test/groovy/org/example/echo/SendAMessageStepDefinitions.groovy
@@ -1,25 +1,31 @@
 package org.example.echo
 
 import cucumber.api.PendingException
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+import org.example.shared.BaseStepDefinition
 
 /**
- * Implementation of the 'retrieve a message' scenario.
+ * Implementation of the upload message scenario.
  */
+class SendAMessageStepDefinitions extends BaseStepDefinition {
 
-this.metaClass.mixin(cucumber.api.groovy.Hooks)
-this.metaClass.mixin(cucumber.api.groovy.EN)
+    @Given( '^a text message I want to store$' )
+    void a_text_message_I_want_to_store() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException()
+    }
 
-Given(~/^a text message I want to store$/) { ->
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
-}
+    @When( '^I call the echo message service$' )
+    void i_call_the_echo_message_service() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException()
+    }
 
-When(~/^I call the echo message service$/) { ->
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
-}
-
-Then(~/^my message should be stored in the system$/) { ->
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
+    @Then( '^my message should be stored in the system$' )
+    void my_message_should_be_stored_in_the_system() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException()
+    }
 }

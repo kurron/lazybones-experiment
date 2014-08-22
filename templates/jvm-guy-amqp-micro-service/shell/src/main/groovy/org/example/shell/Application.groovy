d7f33b829ade147055915d66ff142f505a89d19b
Ron Kurr <rkurr@transparent.com>
2014-08-22 11:25:13 -0400

    Added configuration support for RabbitMQ.

--- templates/jvm-guy-amqp-micro-service/shell/src/main/groovy/org/example/shell/Application.groovy
+++ templates/jvm-guy-amqp-micro-service/shell/src/main/groovy/org/example/shell/Application.groovy
@@ -1,17 +1,26 @@
 package org.example.shell
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.core.AmqpAdmin
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitAdmin
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 import org.springframework.shell.Bootstrap
 
 /**
  * Application driver.
  */
+@SuppressWarnings( 'GrMethodMayBeStatic' )
 @ComponentScan( ['org.example'] )
 @Configuration
+@PropertySource( 'classpath:/config/application.properties' )
 class Application {
 
     /**
@@ -25,8 +34,35 @@ class Application {
     @Autowired
     private ApplicationProperties properties
 
+    // Necessary because Spring does not create one automatically for you.
+    @Bean
+    static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        new PropertySourcesPlaceholderConfigurer()
+    }
+
     @Bean
     ObjectMapper objectMapper() {
         new ObjectMapper()
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        def bean = new CachingConnectionFactory( properties.rabbitHost, properties.rabbitPort )
+        bean.publisherConfirms = false
+        bean.publisherReturns = false
+        bean.username = properties.rabbitUsername
+        bean.password = properties.rabbitPassword
+        bean.virtualHost = properties.rabbitVirtualHost
+        bean
+    }
+
+    @Bean
+    AmqpAdmin amqpAdmin() {
+        new RabbitAdmin( connectionFactory() )
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate() {
+        new RabbitTemplate( connectionFactory() )
     }
 }
--- templates/jvm-guy-amqp-micro-service/shell/src/main/groovy/org/example/shell/ApplicationProperties.groovy
+++ templates/jvm-guy-amqp-micro-service/shell/src/main/groovy/org/example/shell/ApplicationProperties.groovy
@@ -1,14 +1,12 @@
 package org.example.shell
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Component
 
 /**
  * Application specific properties.
  */
 @Component
-@PropertySource( 'classpath:/config/application.properties' )
 class ApplicationProperties {
     /**
      * The name of the AMQP queue where messages go to.
@@ -21,4 +19,35 @@ class ApplicationProperties {
      */
     @Value( '${application.id}' )
     String applicationID = 'unset'
+
+    /**
+     * Host name or ip address of the RabbitMQ server.
+     */
+    @Value( '${rabbitmq.host}' )
+    String rabbitHost = 'unset'
+
+    /**
+     * Port of the RabbitMQ server.
+     */
+    @Value( '${rabbitmq.port}' )
+    int rabbitPort = Integer.MIN_VALUE
+
+    /**
+     * Virtual host to use.
+     */
+    @Value( '${rabbitmq.virtualHost}' )
+    String rabbitVirtualHost = 'unset'
+
+    /**
+     * Username to use when communicating with RabbitMQ.
+     */
+    @Value( '${rabbitmq.username}' )
+    String rabbitUsername = 'unset'
+
+    /**
+     * Password to use when communicating with RabbitMQ.
+     */
+    @Value( '${rabbitmq.password}' )
+    String rabbitPassword = 'unset'
+
 }

47fb56df0ba66ebb77330eb6f8fca1f71fd93075
Ron Kurr <rkurr@transparent.com>
2014-09-18 20:31:43 -0400

    Fixed the runtime issues.

--- templates/jvm-guy-amqp-micro-service/service/src/main/groovy/org/example/echo/EchoDocumentRepositoryImpl.groovy
+++ templates/jvm-guy-amqp-micro-service/service/src/main/groovy/org/example/echo/EchoDocumentRepositoryImpl.groovy
@@ -1,14 +1,15 @@
 package org.example.echo
 
-import groovy.transform.Canonical
-import groovy.transform.InheritConstructors
 import org.example.shared.BaseRepository
+import org.springframework.data.mongodb.core.MongoOperations
 
 /**
  * Implements any custom extensions to the repository.  The name must end in Impl in order for
  * Spring Data MongoDB to find it during startup.
  */
-@InheritConstructors
-@Canonical
 class EchoDocumentRepositoryImpl extends BaseRepository implements EchoDocumentRepositoryExtension {
+
+    EchoDocumentRepositoryImpl( MongoOperations aTemplate ) {
+        super( aTemplate )
+    }
 }

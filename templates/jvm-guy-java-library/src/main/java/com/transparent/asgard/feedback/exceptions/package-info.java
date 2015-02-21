17e2763583ae8ed90a5701cee1092013fb0c0f6f
Ron Kurr <rkurr@transparent.com>
2015-02-21 14:11:53 -0500

    Renamed the package.

--- templates/jvm-guy-java-library/src/main/java/com/transparent/asgard/feedback/exceptions/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2015 Transparent Language.  All rights reserved.
- */
-
-/**
- * Contains basic error classes based on HTTP status codes, which can be extended and customized as needed.
- */
-package com.transparent.asgard.feedback.exceptions;
--- templates/jvm-guy-java-library/src/main/java/com/transparent/asgard/feedback/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2015 Transparent Language.  All rights reserved.
- */
-
-/**
- * The root package of the project, containing all feedback and error related classes.
- */
-package com.transparent.asgard.feedback;

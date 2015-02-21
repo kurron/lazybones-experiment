b9600e36706e0ba38a954a53c66949334a4f096b
Ron Kurr <rkurr@transparent.com>
2015-02-21 16:43:54 -0500

    Gutted the template

--- templates/jvm-guy-rest-api/service/src/main/groovy/org/example/echo/EchoDocumentRepositoryExtension.groovy
+++
@@ -1,8 +0,0 @@
-package org.example.echo
-
-/**
- * Custom repository methods, such as queries too complex to rely on Spring Data MongoDB's naming scheme
- * or if you use MongoDB's aggregation framework.
- */
-interface EchoDocumentRepositoryExtension {
-}

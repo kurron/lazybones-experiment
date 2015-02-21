8a6f770a3f845092326d49030d0071aa878cfb91
Ron Kurr <rkurr@transparent.com>
2015-02-21 14:04:01 -0500

    Simplified dependencies a bit.

--- templates/jvm-guy-java-library/bin/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This script will launch Magni using the default profile
-$JAVA_HOME/bin/java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=dumponexit=true,defaultrecording=true,dumponexitpath=/tmp/magni.jfr -jar build/libs/magni-0.0.0-SNAPSHOT.jar

da3c02d9b2f6130abb8cc6a6695cc1b371b67d81
Ron Kurr <kurr@kurron.org>
2015-07-29 00:44:11 +0000

    Made CodeNarc happy.

--- templates/jvm-guy-aws-lambda/src/main/groovy/org/kurron/example/lambda/RequestResponseLambda.groovy
+++ templates/jvm-guy-aws-lambda/src/main/groovy/org/kurron/example/lambda/RequestResponseLambda.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.kurron.example.lambda
 
 import com.amazonaws.services.lambda.runtime.Context
@@ -25,8 +24,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler
 class RequestResponseLambda implements RequestHandler<Request, Response> {
 
     @Override
-    Response handleRequest( final Request input, final Context context )
-    {
+    Response handleRequest( final Request input, final Context context ) {
         new Response( greetings: "Hello, ${input.firstName} ${input.lastName}" )
     }
 }

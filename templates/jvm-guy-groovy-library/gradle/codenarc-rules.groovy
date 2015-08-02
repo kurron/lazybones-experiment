ba9fb57f4fef3d2934bfdea36040e2a4428b442d
Ron Kurr <kurr@kurron.org>
2015-08-02 14:28:05 +0000

    Got rid of more silliness

--- templates/jvm-guy-groovy-library/gradle/codenarc-rules.groovy
+++ templates/jvm-guy-groovy-library/gradle/codenarc-rules.groovy
@@ -345,10 +345,14 @@ ruleset {
     SerializableClassMustDefineSerialVersionUID
 
     // rulesets/size.xml
-    AbcMetric   // Requires the GMetrics jar
+/*
+    AbcMetric // Requires the GMetrics jar
+*/
     ClassSize
+/*
     CrapMetric   // Requires the GMetrics jar and a Cobertura coverage file
     CyclomaticComplexity   // Requires the GMetrics jar
+*/
     MethodCount
     MethodSize
     NestedBlockDepth

1638807213aa3f2e6b00370ad627a4e9f689ff40
Ron Kurr <kurr@kurron.org>
2015-08-22 21:31:36 +0000

    More fun with generated and defaulted values.

--- templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/Parent.groovy
+++ templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/Parent.groovy
@@ -1,14 +1,19 @@
 package org.kurron.example.jpa.persistence
 
 import groovy.transform.Canonical
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
 import javax.persistence.Transient
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 import org.hibernate.annotations.ColumnTransformer
 import org.hibernate.annotations.Formula
+import org.hibernate.annotations.Generated as HibernateGenerated
+import org.hibernate.annotations.GenerationTime
 
 /**
  * This is an entity, meaning it can stand on its own, that can be associated to 1 or more children.
@@ -37,4 +42,11 @@ class Parent {
     @ColumnTransformer( read = 'IMPERIALWEIGHT / 2.20462', write = '? * 2.20462' )
     int imperialWeight
 
+    @Temporal( TemporalType.TIMESTAMP )
+    @HibernateGenerated( GenerationTime.ALWAYS )
+    Calendar lastModified
+
+    @Column( insertable = false, columnDefinition = "default 'Hello, there.'" )
+    @HibernateGenerated( GenerationTime.INSERT )
+    String alwaysTheSame
 }

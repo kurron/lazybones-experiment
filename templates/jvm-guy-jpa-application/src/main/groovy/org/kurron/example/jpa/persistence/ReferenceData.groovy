2eb5280cc87a6822ccd10ccf56c29e4511ebad2f
Ron Kurr <kurr@kurron.org>
2015-08-22 21:11:45 +0000

    Added subselect support.

--- templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/ReferenceData.groovy
+++ templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/ReferenceData.groovy
@@ -3,13 +3,18 @@ package org.kurron.example.jpa.persistence
 import groovy.transform.Canonical
 import javax.persistence.Entity
 import org.hibernate.annotations.Immutable as HibernateImmutable
+import org.hibernate.annotations.Subselect
+import org.hibernate.annotations.Synchronize
 
 /**
- * This is an immutable structure that is only read from the database.
+ * This is an immutable structure that is only read from the database.  The data is filled using a sub-select of a
+ * larger table.  Our database admin won't let us create a view!
  */
 @Canonical
 @Entity
 @HibernateImmutable
+@Subselect( value = 'select i.ID from ITEM' )
+@Synchronize( ['Item', 'Bid'] )
 class ReferenceData {
 
     String name

79452bfd67cb5eaafbd2ce3bf38d0ce252506374
Ron Kurr <kurr@kurron.org>
2015-08-22 21:25:12 +0000

    Added calculated column support

--- templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/Parent.groovy
+++ templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/Parent.groovy
@@ -4,8 +4,11 @@ import groovy.transform.Canonical
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.Transient
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
+import org.hibernate.annotations.ColumnTransformer
+import org.hibernate.annotations.Formula
 
 /**
  * This is an entity, meaning it can stand on its own, that can be associated to 1 or more children.
@@ -19,6 +22,19 @@ class Parent {
     Long id
 
     @NotNull
-    @Size( min = 1, max = 255, message = 'Name is required, 255 character maximum')
+    @Size( min = 2, max = 255, message = 'Name is required, 255 character maximum')
     String name
+
+    @Transient
+    String doNotSaveMe = 'This should not be stored in the database'
+
+    // this is a read-only derived property using database functions to fill the value at load time
+    @Formula( 'substr( name, 1, 2 )' )
+    String shortName
+
+    // we store weight in pounds but the application wants kilograms
+    // be careful in queries since there is no index for this "column" -- full table scans!
+    @ColumnTransformer( read = 'IMPERIALWEIGHT / 2.20462', write = '? * 2.20462' )
+    int imperialWeight
+
 }

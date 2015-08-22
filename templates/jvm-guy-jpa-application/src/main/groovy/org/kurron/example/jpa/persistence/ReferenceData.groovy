2b6c2ebbd0a0f1e099fc4a913cf69bc755d86313
Ron Kurr <kurr@kurron.org>
2015-08-22 21:12:15 +0000

    Added subselect support.

--- templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/ReferenceData.groovy
+++ templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/ReferenceData.groovy
@@ -2,6 +2,8 @@ package org.kurron.example.jpa.persistence
 
 import groovy.transform.Canonical
 import javax.persistence.Entity
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 import org.hibernate.annotations.Immutable as HibernateImmutable
 import org.hibernate.annotations.Subselect
 import org.hibernate.annotations.Synchronize
@@ -17,5 +19,7 @@ import org.hibernate.annotations.Synchronize
 @Synchronize( ['Item', 'Bid'] )
 class ReferenceData {
 
+    @NotNull
+    @Size( min = 1, max = 255, message = 'Name is required, 255 character maximum')
     String name
 }

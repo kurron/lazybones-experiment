befa1a3dde85c2475b0b63d44ab65dcfa98fb89b
Ron Kurr <kurr@kurron.org>
2015-08-23 20:46:22 +0000

    Use Y or N instead of 1/0 for booleans.

--- templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/Child.groovy
+++ templates/jvm-guy-jpa-application/src/main/groovy/org/kurron/example/jpa/persistence/Child.groovy
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
-
+import org.hibernate.annotations.Type as HibernateType
 /**
  * This is an entity, meaning it can stand on its own, that can be associated to 1 or more parents.
  */
@@ -24,4 +24,7 @@ class Child {
 
     // no annotation needed because the Address class is annotated
     Address address
+
+    @HibernateType( type = 'yes_no' )
+    boolean useYesNo
 }

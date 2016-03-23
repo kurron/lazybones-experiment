123f4fb94664baa26b74588c9979b3537ef7cbe2
Ron Kurr <kurr@kurron.org>
2016-03-23 19:56:28 +0000

    Outbound gateway is working and tested.

--- templates/jvm-guy-spring-boot-project/src/main/groovy/org/kurron/example/Application.groovy
+++ templates/jvm-guy-spring-boot-project/src/main/groovy/org/kurron/example/Application.groovy
@@ -4,12 +4,14 @@ import groovy.util.logging.Slf4j
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 
 /**
  * The entry point into the system.  Runs as a standalone web server.
  */
 @Slf4j
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableConfigurationProperties( ApplicationProperties )
 class Application {
 

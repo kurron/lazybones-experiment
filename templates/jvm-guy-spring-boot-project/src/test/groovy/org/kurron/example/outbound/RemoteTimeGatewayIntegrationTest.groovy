088f7d6586d2d516e865dbb550663973c671ebeb
Ron Kurr <kurr@kurron.org>
2016-03-25 19:01:18 +0000

    We now pull the configuration from a configuration server.

--- templates/jvm-guy-spring-boot-project/src/test/groovy/org/kurron/example/outbound/RemoteTimeGatewayIntegrationTest.groovy
+++ templates/jvm-guy-spring-boot-project/src/test/groovy/org/kurron/example/outbound/RemoteTimeGatewayIntegrationTest.groovy
@@ -1,6 +1,5 @@
 package org.kurron.example.outbound
 
-import java.time.Instant
 import org.junit.experimental.categories.Category
 import org.kurron.categories.OutboundIntegrationTest
 import org.kurron.example.Application
@@ -12,13 +11,15 @@ import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
+import java.time.Instant
+
 /**
  * An integration-level test of the RemoteTimeGateway object.
  **/
 @Category( OutboundIntegrationTest )
 @IntegrationTest
 @ContextConfiguration( classes = Application, loader = SpringApplicationContextLoader )
-class SampleGatewayIntegrationTest extends Specification implements GenerationAbility {
+class RemoteTimeGatewayIntegrationTest extends Specification implements GenerationAbility {
 
     @Autowired
     @Qualifier( 'production' )

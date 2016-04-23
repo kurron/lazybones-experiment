62faf40bed90bb5137b571f4ef0401df25356b2e
Ron Kurr <kurr@kurron.org>
2016-04-23 17:50:40 -0400

    Sanity check test works!

--- templates/jvm-guy-spring-boot-project/src/main/groovy/org/kurron/example/ApplicationProperties.groovy
+++ templates/jvm-guy-spring-boot-project/src/main/groovy/org/kurron/example/ApplicationProperties.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kurron.example
 
 import org.springframework.boot.context.properties.ConfigurationProperties
@@ -7,6 +23,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties
  */
 @ConfigurationProperties( value = 'example', ignoreUnknownFields = false )
 class ApplicationProperties {
+
+    /**
+     * Identifies this type of service. Used in logging.
+     */
+    String serviceCode
+
+    /**
+     * Identifies this instance of the service. Used in logging.
+     */
+    String serviceInstance
+
+    /**
+     * Logically groups a collection of services. Used in logging.
+     */
+    String realm
 
     /**
      * This property controls...
@@ -37,9 +68,4 @@ class ApplicationProperties {
      * This property controls how many times a message will be processed before being declared a poison message.
      */
     int messageRetryAttempts
-
-    /**
-     * What to name the message stream channel.
-     */
-    String inputChannelName
 }

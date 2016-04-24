dbadc71916d9c81a45f250f4b1e211bde01202bc
Ron Kurr <kurr@kurron.org>
2016-04-24 15:27:31 -0400

    We now validate incoming data.

--- templates/jvm-guy-spring-boot-project/src/main/groovy/org/kurron/example/inbound/rest/RestGateway.groovy
+++ templates/jvm-guy-spring-boot-project/src/main/groovy/org/kurron/example/inbound/rest/RestGateway.groovy
@@ -1,23 +1,42 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kurron.example.inbound.rest
 
+import java.time.Instant
+import javax.servlet.http.HttpServletRequest
+import javax.validation.Valid
 import org.kurron.example.MessagingContext
 import org.kurron.example.core.TimeComponent
 import org.kurron.feedback.AbstractFeedbackAware
 import org.kurron.stereotype.InboundRestGateway
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.hateoas.Link
+import org.springframework.hateoas.mvc.ControllerLinkBuilder
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.Errors
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.servlet.HandlerMapping
 
-import javax.servlet.http.HttpServletRequest
-import java.time.Instant
-
 /**
  * Inbound HTTP gateway that supports the FIXME resource.
  **/
+@SuppressWarnings( 'GroovyUnusedDeclaration' )
 @InboundRestGateway
 @RequestMapping
 class RestGateway extends AbstractFeedbackAware {
@@ -35,7 +54,7 @@ class RestGateway extends AbstractFeedbackAware {
     @RequestMapping( path = '/descriptor/application', method = [RequestMethod.GET], produces = [HypermediaControl.MIME_TYPE] )
     ResponseEntity<HypermediaControl> fetchApplicationList( HttpServletRequest request ) {
         def control = defaultControl( request )
-        control.add( new Link( 'foo', 'bar' ) )
+        control.add( ControllerLinkBuilder.linkTo( RestGateway ).withSelfRel() )
         control.time = theComponent.currentTime().toString()
         ResponseEntity.ok( control )
     }
@@ -50,7 +69,38 @@ class RestGateway extends AbstractFeedbackAware {
         throw new ForcedApplicationError( MessagingContext.FORCED_ERROR )
     }
 
-    protected static HypermediaControl defaultControl( HttpServletRequest request ) {
+    @RequestMapping( path = '/descriptor/application', method = [RequestMethod.PUT],  consumes = [HypermediaControl.MIME_TYPE], produces = [HypermediaControl.MIME_TYPE] )
+    ResponseEntity<HypermediaControl> showcaseValidationHandling( @RequestBody @Valid HypermediaControl input,
+                                                                                      Errors errors,
+                                                                                      HttpServletRequest request ) {
+        final ResponseEntity<HypermediaControl> response
+        def control = defaultControl( request )
+        control.add( ControllerLinkBuilder.linkTo( RestGateway ).withSelfRel() )
+
+        ResponseEntity.ok( control )
+
+        if ( errors.hasErrors() ) {
+            addErrors( errors, control )
+            response = ResponseEntity.badRequest().body( control )
+        }
+        else {
+            control.time = theComponent.currentTime().toString()
+            response = ResponseEntity.ok( control )
+        }
+        response
+    }
+
+    private static HypermediaControl addErrors( Errors errors, HypermediaControl toAugment ) {
+        toAugment.status = HttpStatus.BAD_REQUEST.value()
+        def validationErrors = errors.fieldErrors.collect { new ValidationError( field: it.field, reason: it.defaultMessage ) }
+        toAugment.errorBlock = new ErrorBlock( code: MessagingContext.VALIDATION_ERROR.code,
+                                               message: 'The uploaded descriptor is invalid.  Please correct the issues and try again.',
+                                               developerMessage: 'Certain properties in the payload are invalid.',
+                                               validationErrors: validationErrors )
+        toAugment
+    }
+
+    private static HypermediaControl defaultControl( HttpServletRequest request ) {
         def path = request.getAttribute( HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE ) as String
         new HypermediaControl( status: HttpStatus.OK.value(), timestamp: Instant.now().toString(), path: path )
     }
--- templates/jvm-guy-spring-boot-project/src/test/groovy/org/kurron/example/inbound/rest/RestGatewayIntegrationTest.groovy
+++ templates/jvm-guy-spring-boot-project/src/test/groovy/org/kurron/example/inbound/rest/RestGatewayIntegrationTest.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kurron.example.inbound.rest
 
 import org.junit.experimental.categories.Category
@@ -9,6 +25,9 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.boot.test.TestRestTemplate
 import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ContextConfiguration
@@ -27,6 +46,26 @@ class RestGatewayIntegrationTest extends Specification implements GenerationAbil
 
     @Autowired
     TestRestTemplate template
+
+    def 'exercise data validation handling'() {
+        given: 'a proper testing environment'
+        assert port
+        assert template
+
+        when: 'we PUT /descriptor/application'
+        def uri = buildURI( port, '/descriptor/application', [:] )
+        def control = new HypermediaControl()
+        def headers = new HttpHeaders()
+        headers.setContentType( HypermediaControl.MEDIA_TYPE )
+        headers.setAccept( [HypermediaControl.MEDIA_TYPE] )
+        def request = new HttpEntity<HypermediaControl>( control, headers )
+        ResponseEntity<HypermediaControl> response = template.exchange( uri, HttpMethod.PUT, request, HypermediaControl)
+
+        then: 'we get a proper response'
+        HttpStatus.BAD_REQUEST == response.statusCode
+        assert response.body.links
+    }
+
 
     def 'exercise GET happy path'() {
         given: 'a proper testing environment'

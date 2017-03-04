c9e181c721a24e80e0c8e9fc9c88da621d92227b
Ron Kurr <kurr@kurron.org>
2017-03-04 13:30:16 -0500

    Enabled validation of the configuration.  Hoping to identify misconfigurations earlier.

--- templates/jvm-guy-spring-boot-messaging-project/src/main/groovy/org/kurron/example/shared/ApplicationProperties.groovy
+++ templates/jvm-guy-spring-boot-messaging-project/src/main/groovy/org/kurron/example/shared/ApplicationProperties.groovy
@@ -15,6 +15,8 @@
  */
 package org.kurron.example.shared
 
+import javax.validation.constraints.NotNull
+import org.hibernate.validator.constraints.NotEmpty
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 /**
@@ -24,73 +26,89 @@ class ApplicationProperties {
 
     /**
      * Contains the logging related properties.*/
+    @NotNull
     Logging logging
 
     /**
      * Contains the dead letter exchange settings.*/
+    @NotNull
     DeadLetter deadletter
 
     /**
      * Contains the inbound AMQP settings.*/
+    @NotNull
     Inbound inbound
 
     /**
      * Contains the outbound AMQP settings.*/
+    @NotNull
     Outbound outbound
 
     static class Logging {
         /**
          * Identifies this type of service. Used in logging.*/
+        @NotEmpty
         String serviceCode
 
         /**
          * Identifies this instance of the service. Used in logging.*/
+        @NotEmpty
         String serviceInstance
 
         /**
          * Logically groups a collection of services. Used in logging.*/
+        @NotEmpty
         String realm
     }
 
     static class DeadLetter {
         /**
          * This property controls the name of the exchange used to publish poison messages to.*/
+        @NotEmpty
         String exchangeName
 
         /**
          * This property controls the queue that is bound to the poison messages exchange.*/
+        @NotEmpty
         String queueName
 
         /**
          * This property controls the routing key that is used when publishing poison messages.*/
+        @NotEmpty
         String routingKey
 
         /**
          * This property controls how many times a message will be processed before being declared a poison message.*/
+        @org.hibernate.validator.constraints.Range( min = 1L, max = 5L )
         int messageRetryAttempts
     }
 
     static class Inbound {
         /**
          * This property controls the name of the exchange that the outbound gateway publishes to.*/
+        @NotEmpty
         String exchangeName
 
         /**
          * This property controls the routing key that the outbound gateway publishes to.*/
+        @NotEmpty
         String routingKey
 
         /**
          * This property controls the queue that is bound to the inbound exchange.*/
+        @NotEmpty
         String queueName
     }
 
     static class Outbound {
         /**
          * This property controls the name of the exchange that the outbound gateway publishes to.*/
+        @NotEmpty
         String exchangeName
 
         /**
          * This property controls the routing key that the outbound gateway publishes to.*/
+        @NotEmpty
         String routingKey
     }
 
@@ -101,6 +119,7 @@ class ApplicationProperties {
     /**
      * A dumb way to test that all messages got processed.
      */
+    @org.hibernate.validator.constraints.Range( min = 1L, max = 10L )
     Integer outstandingMessages
 
 }

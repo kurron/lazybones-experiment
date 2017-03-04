be200a16a2eb822c0220ae3c6dd76687c73b50e2
Ron Kurr <kurr@kurron.org>
2017-03-04 10:26:13 -0500

    Fixed the CodeNarc issues.

--- templates/jvm-guy-spring-boot-messaging-project/bin/simulate-build-server.sh
+++ templates/jvm-guy-spring-boot-messaging-project/bin/simulate-build-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 UNIXTIME=$(date +%s)
-./gradlew -PrunIntegrationTests=true -PpublishArtifacts=true -Pbranch=master -Ppatch=${UNIXTIME}
+./gradlew -PrunIntegrationTests=false -PpublishArtifacts=true -Pbranch=master -Ppatch=${UNIXTIME}

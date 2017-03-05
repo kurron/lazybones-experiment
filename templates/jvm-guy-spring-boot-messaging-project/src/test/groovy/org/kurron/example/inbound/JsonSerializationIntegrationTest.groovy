1a6fb6384559ab76c06366f8e254c3839f144934
Ron Kurr <kurr@kurron.org>
2017-03-05 11:16:59 -0500

    Showcase testing stdout/stderr streams.

--- templates/jvm-guy-spring-boot-messaging-project/src/test/groovy/org/kurron/example/inbound/JsonSerializationIntegrationTest.groovy
+++ templates/jvm-guy-spring-boot-messaging-project/src/test/groovy/org/kurron/example/inbound/JsonSerializationIntegrationTest.groovy
@@ -18,11 +18,13 @@ package org.kurron.example.inbound
 import static org.assertj.core.api.Assertions.assertThat
 import groovy.util.logging.Slf4j
 import java.text.SimpleDateFormat
+import org.junit.Rule
 import org.junit.experimental.categories.Category
 import org.kurron.categories.InboundIntegrationTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.json.JsonTest
 import org.springframework.boot.test.json.JacksonTester
+import org.springframework.boot.test.rule.OutputCapture
 import org.springframework.boot.test.util.EnvironmentTestUtils
 import org.springframework.context.ConfigurableApplicationContext
 import spock.lang.Specification
@@ -41,9 +43,21 @@ class JsonSerializationIntegrationTest extends Specification {
     @Autowired
     private ConfigurableApplicationContext environment
 
+    @Rule
+    private final OutputCapture capture = new OutputCapture()
+
     void setup() {
         // an example of how to introduce a property into the environment prior to the tests running
         EnvironmentTestUtils.addEnvironment( environment, 'org=Spring', 'name=Boot' )
+    }
+
+    @SuppressWarnings( 'Println' )
+    void 'verify we can watch std out'() {
+
+        expect: 'showcase how to watch stdout/stderr'
+        println environment.environment.getProperty( 'org' )
+        // the stream contains other output so you have to do a contains
+        capture.toString().contains( 'Spring' )
     }
 
     void 'verify serialization'() {

73b68707acaa8658d42fc50df914d0425f5158df
Ron Kurr <kurr@kurron.org>
2017-03-05 14:23:53 -0500

    Spit out the stacktrace when the build breaks.

--- templates/jvm-guy-spring-boot-messaging-project/bin/simulate-build-server.sh
+++ templates/jvm-guy-spring-boot-messaging-project/bin/simulate-build-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 UNIXTIME=$(date +%s)
-./gradlew -PrunIntegrationTests=true -PpublishArtifacts=true -Pbranch=master -Ppatch=${UNIXTIME}
+./gradlew -PrunIntegrationTests=true -PpublishArtifacts=true -Pbranch=master -Ppatch=${UNIXTIME} --stacktrace
